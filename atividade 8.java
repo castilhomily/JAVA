625b4ec35b60b165e36eb6ee88f994b5466aae05
Emily Castilho Dias <emilyschool1305@gmail.com>
2026-03-09 20:11:25 -0300

    .

---
+++ atividade 8.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int n = sc.nextInt();
+
+        while (true) {
+            System.out.print(n + " ");
+
+            if (n == 1) {
+                break;
+            }
+
+            if (n % 2 != 0) {
+                n = 3 * n + 1;
+            } else { 
+                n = n / 2;
+            }
+        }
+
+        sc.close();
+    }
+}
