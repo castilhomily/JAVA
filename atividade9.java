b972fc85e6fde17ca1f3116a81d2a9cc61a2d5f8
Emily Castilho Dias <emilyschool1305@gmail.com>
2026-03-09 20:21:56 -0300

    .

--- atividade9.java
+++ atividade9.java
@@ -47,3 +47,21 @@ public class leitura {
         System.out.println(avaliacao);
     }
 }
+
+//condicional 
+public class condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2018;
+        boolean incluindoNoPlano = true;
+        double notaDaSerie = 9.2;
+        String tipoPlano = "normal";
+
+            // código omitido
+
+            if (incluindoNoPlano == true && tipoPlano.equals("plus")){
+                System.out.println("Filme liberado");
+            } else {
+                System.out.println("Deve pagar a locação");
+            }
+        }
+    }
